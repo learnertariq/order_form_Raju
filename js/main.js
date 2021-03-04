72c0d1087b601a24fa21f32726ee98442de4eca5
tariq <youandtariq@gmail.com>
2021-03-04 22:49:27 +0600

    refactored all codes and applied OOP everywhere

--- js/main.js
+++ js/main.js
@@ -1,90 +1,62 @@
 $(".chosen").chosen();
-var sel = document.querySelector(".product-list");
-var productAddButton = document.querySelector(".product-add-button");
-var totalPrice = document.querySelector(".product-prices__total");
-var inputs;
-var productSelected = sel.options[sel.selectedIndex];
-var total = document.querySelector(".total-prices-row");
 
-// listen touch event
-productAddButton.addEventListener("touchstart", (ev) => {
-  productSelected = sel.options[sel.selectedIndex];
+// Global Variables
+var productUpdateBtn = document.querySelector(".product__update-btn");
+var productSelectBtn = document.querySelector(".product__select-btn");
+var productSelectOptions = document.querySelector(".product__select-options");
+var productPriceInputs = document.querySelectorAll(".product__price .input");
+var productRowTotal = document.querySelector(".product__row-total");
+var productPriceTotal = document.querySelector(".product__price-total .input");
+var productRemoveBtn = document.querySelector(".product__remove-btn");
 
-  addNewProduct(productSelected);
-
-  //updating inputs
-  inputs = document.querySelectorAll(".input-product");
-});
-
-// listen click event
-productAddButton.addEventListener("click", (ev) => {
-  productSelected = sel.options[sel.selectedIndex];
-
-  addNewProduct(productSelected);
-
-  //updating inputs
-  inputs = document.querySelectorAll(".input-product");
-});
-
-// add new product Html
- function addNewProduct(productSelected) {
-  total.insertAdjacentHTML(
-    "beforebegin", 
-    `<tr class="product-prices__item">
-      <td class="product-prices__name">${productSelected.text}</td>
-      <td class="product-prices__price"><input class="input input-product" type="number" value="${productSelected.value}"></td>
-      <td><button class="btn btn--xsmall btn--primary product-remove-btn">rm</button></td>
-    </tr>`);
-
-    totalPrice.value = parseInt(totalPrice.value) + parseInt(productSelected.value);
- }
-
-
-// calculating total prices
-function recalculateTotal() {
-  var total = 0;
-  for (var i = 0; i < inputs.length; i++) {
-    total += parseInt(inputs[i].value);
-  }
-  totalPrice.value = total;
-}
-
-//updating prices on change explicitly CLICK
-document.querySelector(".update-prices-btn").addEventListener('click', () => {
+// Adding New Product
+productSelectBtn.addEventListener('click', (ev) => {
+  addNewProductHtml();
+  // update total price
   recalculateTotal();
 });
 
-//updating prices on change explicitly TOUCH
-document.querySelector(".update-prices-btn").addEventListener('touchstart', () => {
-  recalculateTotal();
-});
+// update total on change
+productUpdateBtn.addEventListener('click', () => recalculateTotal());
 
-
-// removing product row CLICK
+// removing product row
 document.addEventListener('click', (ev) => {
   console.log(ev)
-  if (ev.target.classList.contains("product-remove-btn")) {
+  if (ev.target.classList.contains("product__remove-btn")) {
     ev.target.parentElement.parentElement.remove();
 
-    //updating inputs
-    inputs = document.querySelectorAll(".input-product");
-
-    recalculateTotal();
-  }
-});
-
-// removing product row TOUCH
-document.addEventListener('touchstart', (ev) => {
-  console.log(ev)
-  if (ev.target.classList.contains("product-remove-btn")) {
-    ev.target.parentElement.parentElement.remove();
-
-    //updating inputs
-    inputs = document.querySelectorAll(".input-product");
-
+    updateInputs();
     recalculateTotal();
   }
 });
 
 
+//////////// utility functions  /////////////////
 
+function addNewProductHtml() {
+  var productSelected = productSelectOptions.options[productSelectOptions.selectedIndex];
+  var newProductHtml =
+    `<tr class="product__row">
+      <td class="product__name"><button class="btn btn--xsmall btn--primary product__remove-btn">X</button> ${productSelected.text}</td>
+      <td class="product__price"><input class="input" type="number" value="${productSelected.value}"></td>
+    </tr>`;
+  productRowTotal.insertAdjacentHTML('beforebegin', newProductHtml);
+
+  updateInputs();
+}
+
+// recalculating total prices
+function recalculateTotal() {
+  var total = 0;
+  for (var i = 0; i < productPriceInputs.length; i++) {
+    total += parseInt(productPriceInputs[i].value);
+  }
+  productPriceTotal.value = total;
+  console.log(total);
+  console.log(productPriceTotal);
+}
+
+function updateInputs() {
+  //Updating price inputs
+  productPriceInputs = document.querySelectorAll(".product__price .input");
+}
